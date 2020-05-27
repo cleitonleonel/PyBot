4bc1c96b487c914c930227e52d1b82644c7dc790
cleitonLC <cleiton.leonel@gmail.com>
2020-05-27 00:28:06 -0300

    web bot...

--- core.py
+++ core.py
@@ -150,7 +150,7 @@ def main():
     updater.start_webhook(listen="0.0.0.0",
                           port=int(PORT),
                           url_path=TOKEN)
-    updater.bot.setWebhook('https://yourherokuappname.herokuapp.com/' + TOKEN)
+    updater.bot.setWebhook('https://robot-dev.herokuapp.com/' + TOKEN)
 
     # Block until the user presses Ctrl-C or the process receives SIGINT,
     # SIGTERM or SIGABRT. This should be used most of the time, since

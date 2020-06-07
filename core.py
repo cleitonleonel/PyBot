a032f623ced0bcbf9a0d07408f480a3d254c95e1
cleitonLC <cleiton.leonel@gmail.com>
2020-06-07 19:03:43 -0300

    Lista de filmes atualizada...

--- core.py
+++ core.py
@@ -36,7 +36,7 @@ def search(update, context):
     else:
         parameter = parameter.capitalize()
 
-    data = requests.get('https://gist.githubusercontent.com/cleitonleonel/3d894a0b2d0643d7918b095fccdae047/raw/27a9016a8bccbef980f23bac58013cf979b79de7/filmes.json').json()
+    data = requests.get('https://raw.githubusercontent.com/cleitonleonel/Extractor/master/filmes.json').json()
 
     films = []
     for i, item in enumerate(data):
